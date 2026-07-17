2bfb5dcd912cfafc26d7f664bbcdbe167cdb2578
Shanthosini k <shanthosinik@.com>
2026-07-17 12:26:23 +0530

    Add blogapp under week-5

---
+++ week-5/blogapp/src/Posts.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ posts: data });
+      })
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert('An error occurred: ' + error.toString());
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+        {this.state.posts.map(post => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
